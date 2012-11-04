3c40dac245d621b7a5479dcf78f6143f4b2ba4eb
Chet Lemon <clemon@ucsd.edu>
2012-11-03 20:34:49 -0700

    Google Maps; click to add points along the path;
    Click "send coordnates" button to send
    coordinate list via Flask server
    localhost:5000 (server.py)

--- Google Maps/server.py
+++ Google Maps/server.py
@@ -1,12 +1,27 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 app = Flask(__name__)
 
 app.config["SERVER_NAME"] = "localhost:5000"
 
 @app.route("/", methods=['GET', 'POST'])
-def hello():
-    return "Hello World!"
+def getFlightPlan():
+	listt = []
+	index = 0
+
+	while True:
+		lat = "path[" + str(index) +"][lat]"
+		lng = "path["+ str(index) +"][lng]"
+		if lat in request.form.keys() and lng in request.form.keys():
+			listt += [(request.form[lat],request.form[lng])]
+		else:
+			break 
+		index += 1
+
+	return str(listt)
+			
+
 with app.app_context():
 	url_for('static', filename='map.html')
 if __name__ == "__main__":
-    app.run()
+	app.run()
+
