5fa6d38fa3127cb977207ac65e48a128ebe7a8ae
Chet Lemon <clemon@ucsd.edu>
2013-02-17 16:48:37 -0800

    added sethome to server.py

--- gmappy/server.py
+++ gmappy/server.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 app.config["SERVER_NAME"] = "localhost:5000"
 
-@app.route("/", methods=['GET', 'POST'])
-def getFlightPlan():
-    list = []
+@app.route("/flightplan", methods=['POST'])
+def setFlightPlan():
+    plan = []
     index = 0
 
     while True:
@@ -18,7 +18,7 @@ def getFlightPlan():
         altitude = "data[" + str(index) + "][altitude]"
         duration = "data[" + str(index) + "][duration]"
         if lat in request.form.keys():
-            list += [{
+            plan += [{
                 "latitude": request.form[lat],
                 "longitude": request.form[lng],
                 "altitude" : request.form[altitude],
@@ -29,7 +29,7 @@ def getFlightPlan():
             break
         index += 1
 
-    for coordDict in list:
+    for coordDict in plan:
         print "latitude: " + str(coordDict["latitude"])
         print "longitude: " + str(coordDict["longitude"])
         print "altitude: " + str(coordDict["altitude"])
@@ -38,9 +38,21 @@ def getFlightPlan():
         print
 
     print "before loading waypoints"
-    mavproxy_headless.load_waypoints_from_array(list)
+    mavproxy_headless.load_waypoints_from_array(plan)
     print "after loading from waypoints"
 
     mavproxy_headless.save_waypoints("output.txt")
 
     return "OK"
+
+@app.route("/home", methods=['POST'])
+def setHome():
+    lat=float(request.form["lat"])
+    lng=float(request.form["lng"])
+    alt=float(request.form["alt"])
+
+    mavproxy_headless.master().command_long_send(mpstate.status.target_system, 
+                                                mpstate.status.target_component,
+                                                MAV_CMD_DO_SET_HOME,
+                                                1, #comfirmation
+                                                lat, lng, alt)
